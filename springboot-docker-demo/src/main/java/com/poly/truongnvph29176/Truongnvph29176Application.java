82969b1c7efc4213d0944a788f8f5e9634e4dff8
truongnv02 <nguyenvantruong200602@gmail.com>
2023-11-27 11:18:54 +0700

    update

--- springboot-docker-demo/src/main/java/com/poly/truongnvph29176/Truongnvph29176Application.java
+++ springboot-docker-demo/src/main/java/com/poly/truongnvph29176/Truongnvph29176Application.java
@@ -2,9 +2,17 @@ package com.poly.truongnvph29176;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Truongnvph29176Application {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Spring Boot Docker Demo";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Truongnvph29176Application.class, args);
